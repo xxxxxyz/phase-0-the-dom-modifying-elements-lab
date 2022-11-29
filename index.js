d1afdf24ddd1c65555f8964a6b7a6f427972fd76
yuzexia <xiayuze0406@gmail.com>
2022-11-29 17:36:20 +1100

    Done

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const testVar = "hi"
+
+// Remove Element from the  DOM
+
+document.querySelector("main#main").remove();
+
+// Has a newHeader variable that points to an h1 node
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "Bushrangeers is the champion";
